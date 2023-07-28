e1b2ce163a4241aea6cdac04f4d4495e25aa4144
christopher-w-murphy <murphtron5000@gmail.com>
2023-07-28 10:49:09 -0400

    improved numeric stability of softmax_n function

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -8,7 +8,12 @@ from src.activation import softmax_n, softmax_1
 
 @fixture(scope='session')
 def expected_numerators():
-    return [[1, 3, 6], [4, 1, 5]]
+    return [[1, 3, 6], [3, 1, 4]]
+
+
+@fixture(scope='session')
+def expected_denominators():
+    return [10, 8]
 
 
 @fixture(scope='session')
@@ -16,12 +21,12 @@ def input_data(expected_numerators):
     return log(Tensor(expected_numerators))
 
 
-def test_softmax_n(input_data, expected_numerators):
+def test_softmax_n(input_data, expected_numerators, expected_denominators):
     output_0_builtin = softmax(input_data, dim=-1)
     assert output_0_builtin.size() == input_data.size()
     for idx in range(2):
         for jdx in range(3):
-            assert output_0_builtin[idx][jdx].item() == approx(expected_numerators[idx][jdx]/ 10)
+            assert output_0_builtin[idx][jdx].item() == approx(expected_numerators[idx][jdx] / expected_denominators[idx])
 
     output_0 = softmax_n(input_data, 0)
     assert output_0.size() == input_data.size()
@@ -33,18 +38,18 @@ def test_softmax_n(input_data, expected_numerators):
     assert output_1.size() == input_data.size()
     for idx in range(2):
         for jdx in range(3):
-            assert output_1[idx][jdx].item() == approx(expected_numerators[idx][jdx] / 11)
+            assert output_1[idx][jdx].item() == approx(expected_numerators[idx][jdx] / (expected_denominators[idx] + 1))
 
     output_4 = softmax_n(input_data, 4)
     assert output_4.size() == input_data.size()
     for idx in range(2):
         for jdx in range(3):
-            assert output_4[idx][jdx].item() == approx(expected_numerators[idx][jdx] / 14)
+            assert output_4[idx][jdx].item() == approx(expected_numerators[idx][jdx] / (expected_denominators[idx] + 4))
 
 
-def test_softmax_1(input_data, expected_numerators):
+def test_softmax_1(input_data, expected_numerators, expected_denominators):
     output_1 = softmax_1(input_data)
     assert output_1.size() == input_data.size()
     for idx in range(2):
         for jdx in range(3):
-            assert output_1[idx][jdx].item() == approx(expected_numerators[idx][jdx] / 11)
+            assert output_1[idx][jdx].item() == approx(expected_numerators[idx][jdx] / (expected_denominators[idx] + 1))
