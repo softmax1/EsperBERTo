681b6667a1df412f6aa6b9a1e53c30320c6ab457
christopher-w-murphy <murphtron5000@gmail.com>
2023-07-31 20:43:39 -0400

    softmax_n now has all the arguments of F.softmax

--- src/functional.py
+++ src/functional.py
@@ -1,41 +1,18 @@
-from torch import Tensor, index_select, arange, exp, divide, subtract, multiply, add
+from typing import Optional
+
+from torch import Tensor, index_select, arange, exp, divide, subtract, multiply, add, clip
 from torch.cuda import is_available
-from torch.nn import ConstantPad1d
-from torch.nn.functional import softmax
+from torch.nn.functional import softmax, pad
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from torch.types import _dtype as DType
+else:
+    # The JIT doesn't understand Union, nor torch.dtype here
+    DType = int
 
 
-def softmax_n_shifted_zeros(input: Tensor, n: int) -> Tensor:
-    """
-    $\text(softmax)_n(x_i) = exp(x_i) / (n + \sum_j exp(x_j))$
-
-    Note: softmax_n, with fixed input, is _not_ shift-symmetric when n != 0, and we must account for this.
-    Normally when computing a softmax, the maxes are subtracted from the inputs for numeric stability.
-    """
-    # compute the maxes along the last dimension
-    input_maxes = input.max(dim=-1, keepdim=True).values
-    # shift the input to prevent overflow (and underflow in the denominator)
-    shifted_inputs = subtract(input, input_maxes)
-    # compute the numerator and softmax_0 denominator using the shifted input
-    numerator = exp(shifted_inputs)
-    original_denominator = numerator.sum(dim=-1, keepdim=True)
-    # we need to shift the zeros in the same way we shifted the inputs
-    shifted_zeros = multiply(input_maxes, -1)
-    # and then add this contribution to the denominator
-    denominator = add(original_denominator, multiply(exp(shifted_zeros), n))
-    return divide(numerator, denominator)
-
-
-def softmax_1(input: Tensor) -> Tensor:
-    """
-    $\text(softmax)_n(x_i) = exp(x_i) / (1 + \sum_j exp(x_j))$
-
-    After a small amount of testing, the "shifted zeros" approach appears to be faster.
-    I am definitely open to suggestions on which approach is better though.
-    """
-    return softmax_n_shifted_zeros(input, 1)
-
-
-def softmax_n_with_padding(input: Tensor, n: int) -> Tensor:
+def softmax_n_with_padding(input: Tensor, n: int, dim: Optional[int] = None, dtype: Optional[DType] = None) -> Tensor:
     """
     $\text(softmax)_n(x_i) = exp(x_i) / (n + sum_j exp(x_j))$
 
@@ -44,14 +21,67 @@ def softmax_n_with_padding(input: Tensor, n: int) -> Tensor:
     Afterwards we need to un-pad the output.
     """
     # instaniate a padder
-    zero_padding = ConstantPad1d((0, n), 0)
-    # pad the input with n 0s along the last dimension
-    padded_input = zero_padding(input)
+    if dim is None:
+        raise NotImplementedError('The padding approach is currently only implemented for a specific dimension.')
+    if dim >= 0:
+        dim -= len(input.size())
+    padding_size = -(2 * dim + 1) * (0,) + (n,)
+
+    # pad the input with n 0s along the 'dim' dimension
+    padded_input = pad(input, padding_size, value=0)
     # compute the softmax with the padded input
-    padded_output = softmax(padded_input, dim=-1)
+    padded_output = softmax(padded_input, dim=dim)
     # select the indices to keep
     # note that because we're creating this tensor it won't automatically be placed on the correct device
     device = 'cuda' if is_available() else 'cpu'
-    indices_to_keep = arange(input.size()[-1], device=device)
+    indices_to_keep = arange(input.size()[dim], device=device)
     # un-pad the result
-    return index_select(padded_output, dim=-1, index=indices_to_keep)
+    output = index_select(padded_output, dim=dim, index=indices_to_keep)
+    return output if dtype is None else output.type(dtype=dtype)
+
+
+def softmax_1(input: Tensor, dim: Optional[int] = None, dtype: Optional[DType] = None) -> Tensor:
+    """
+    $\text(softmax)_n(x_i) = exp(x_i) / (1 + \sum_j exp(x_j))$
+    """
+    return softmax_n_with_padding(input, 1, dim=dim, dtype=dtype)
+
+
+def softmax_n_shifted_zeros(input: Tensor, n: int, dim: Optional[int] = None, dtype: Optional[DType] = None) -> Tensor:
+    """
+    $\text(softmax)_n(x_i) = exp(x_i) / (n + \sum_j exp(x_j))$
+
+    Note: softmax_n, with fixed input, is _not_ shift-symmetric when n != 0, and we must account for this.
+    Normally when computing a softmax, the maxes are subtracted from the inputs for numeric stability.
+    """
+    # compute the maxes along the last dimension
+    input_maxes = input.max(dim=dim, keepdim=True).values
+    # shift the input to prevent overflow (and underflow in the denominator)
+    shifted_inputs = subtract(input, input_maxes)
+    # compute the numerator and softmax_0 denominator using the shifted input
+    numerator = exp(shifted_inputs)
+    original_denominator = numerator.sum(dim=dim, keepdim=True)
+    # we need to shift the zeros in the same way we shifted the inputs
+    shifted_zeros = multiply(input_maxes, -1)
+    # and then add this contribution to the denominator
+    denominator = add(original_denominator, multiply(exp(shifted_zeros), n))
+    output = divide(numerator, denominator)
+    return output if dtype is None else output.type(dtype=dtype)
+
+
+def softmax_n_naive(input: Tensor, n: int, dim: Optional[int] = None, dtype: Optional[DType] = None) -> Tensor:
+    numerator = exp(input)
+    output = numerator / (numerator.sum(dim=dim, keepdim=True) + n)
+    return output if dtype is None else output.type(dtype=dtype)
+
+
+def clipped_softmax(input: Tensor,
+                    xi: float,
+                    gamma: float,
+                    dim: Optional[int] = None,
+                    dtype: Optional[DType] = None
+                    ) -> Tensor:
+    assert xi >= 1.
+    assert gamma <= 0.
+    output = clip((xi - gamma) * softmax(input, dim=dim) + gamma, 0., 1.)
+    return output if dtype is None else output.type(dtype=dtype)
